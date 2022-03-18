66b57ba596e09e0c72e617f0975b083893054c54
Satish Tiwari <96436404+SatishTiwari1998@users.noreply.github.com>
2022-03-18 16:33:29 +0530

    updtae

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React from "react";
+import "../styles/App.css";
+import { signUpFormValidation } from "../utils/validation.js";
 
 const App = () => {
-  
   return (
-
-  )
-}
-
+    <>
+      <form>
+        <input id="name" />
+        <label id="namelbl"></label>
+        <input id="email" />
+        <label id="emaillbl"></label>
+        <input id="password" type="password" />
+        <label id="passwordlbl"></label>
+        <br />
+        <input type="checkbox" id="consent" /> Consent.
+        <br />
+        <button
+          id="submit"
+          type="submit"
+          onclick={() => {
+            signUpFormValidation();
+          }}
+        >
+          Signup
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default App;
+
